cab684782fb425b500ea22d1c1d98065c1c91800
ngnmsn <ngnmsn@gmail.com>
2022-02-25 05:20:03 +0900

    API getStations Success

--- app/server.go
+++ app/server.go
@@ -1,23 +1,44 @@
 package main
 
 import (
+	"encoding/json"
+	"os"
+	"io/ioutil"
+	"log"
     "net/http"
     "github.com/labstack/echo/v4"
+	"fmt"
 )
 
-type ItemData struct {
-    Id int `json:"id"`
-    Name string `json:"name"`
+type Station struct {
+	Id int `json:"id"`
+	StationName string `json:"stationName"`
+	LineName string `json:"lineName"`
+}
+
+type Response struct {
+	Stations []Station `json:"stations"`
 }
 
 func main() {
     e := echo.New()
-    e.GET("/", func(c echo.Context) error {
-        item := new(ItemData)
-        if err := c.Bind(item); err != nil {
-            return err
-        }
-        return c.JSON(http.StatusOK, item)
+    e.GET("/getStations", func(c echo.Context) error {
+		var stations []Station
+
+		stationsJsonFile, err := os.Open("./TestPackages/json/stations.json")
+	
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer stationsJsonFile.Close()
+
+		stationsByteValue, _ := ioutil.ReadAll(stationsJsonFile)
+		json.Unmarshal(stationsByteValue, &stations)
+		response := new(Response)
+		response.Stations = stations
+		fmt.Println(stations)
+        return c.JSON(http.StatusOK, response)
     })
     e.Logger.Fatal(e.Start(":8080"))
 }
