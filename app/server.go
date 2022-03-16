456e51fdf5949fe2700eafb9d7abfb253d38666f
ngnmsn <ngnmsn@gmail.com>
2022-03-16 22:22:29 +0900

    DB Creation Success

--- app/server.go
+++ app/server.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"os"
-	"io/ioutil"
+	"database/sql"
+	// "encoding/json"
+	// "os"
+	// "io/ioutil"
 	"log"
     "net/http"
     "github.com/labstack/echo/v4"
 	"fmt"
+	_ "github.com/lib/pq"
 )
 
 type Station struct {
@@ -21,22 +23,69 @@ type Response struct {
 }
 
 func main() {
-    e := echo.New()
+
+	var Db *sql.DB
+	Db, err := sql.Open("postgres", "host=postgres port=5432 user=app_user password=app_password dbname=app_db sslmode=disable")
+    if err != nil {
+		log.Fatal(err)
+	}
+
+	// sql := "SELECT id, station_name, line_name FROM STATION_MATSTER_TB WHERE id=$1;"
+
+	// pstatement, err := Db.Prepare(sql)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// queryID := 1
+	// var testStation Station
+
+	// err = pstatement.QueryRow(queryID).Scan(&testStation.Id, &testStation.StationName, &testStation.LineName)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Println(testStation.Id, testStation.StationName, testStation.LineName)
+
+	e := echo.New()
     e.GET("/getStations", func(c echo.Context) error {
+		station := new(Station)
 		var stations []Station
 
-		stationsJsonFile, err := os.Open("./TestPackages/json/stations.json")
+		// stationsJsonFile, err := os.Open("./TestPackages/json/stations.json")
 	
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+
+		// defer stationsJsonFile.Close()
+
+		// stationsByteValue, _ := ioutil.ReadAll(stationsJsonFile)
+		// json.Unmarshal(stationsByteValue, &stations)
+		
+		sql := "SELECT id, station_name, line_name FROM STATION_MATSTER_TB;"
+
+		rows, err := Db.Query(sql)
 		if err != nil {
 			log.Fatal(err)
 		}
+		defer rows.Close()
 
-		defer stationsJsonFile.Close()
+		for rows.Next(){
+			if err := rows.Scan(&station.Id, &station.StationName, &station.LineName); err != nil {
+				log.Fatal(err)
+			}
+			stations = append(stations, Station{
+					Id: station.Id,
+					StationName: station.StationName,
+					LineName: station.LineName,
+				})
+		}
 
-		stationsByteValue, _ := ioutil.ReadAll(stationsJsonFile)
-		json.Unmarshal(stationsByteValue, &stations)
 		response := new(Response)
+
 		response.Stations = stations
+
 		fmt.Println(stations)
 		c.Response().Header().Set(echo.HeaderAccessControlAllowOrigin, "http://localhost:4200")
         return c.JSON(http.StatusOK, response)
---
+++ postgres/init/4_insert_bar_master_tb.sql
@@ -0,0 +1,2 @@
+DELETE FROM BAR_MASTER_TB;
+INSERT INTO BAR_MASTER_TB VALUES(1, 'Craftman Yokohama', 31, '〒221-0835 神奈川県横浜市神奈川区鶴屋町2-16-4', '045-624-8288', 'https://tabelog.com/kanagawa/A1401/A140101/14062941/');
